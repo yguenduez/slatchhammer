1c894e78f6801870e94c69136c0da046c9a5c3a0
yguenduez <yasinguenduez1@gmail.com>
2024-01-12 21:16:18 +0100

    better camera angle

--- src/camera.rs
+++ src/camera.rs
@@ -12,7 +12,7 @@ pub struct MainCamera;
 fn setup_camera(mut commands: Commands) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 18.0, 25.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0.0, 30.0, 25.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         },
         MainCamera,
