a4555d98b966af377ae5e541d0628b072bc9f6cf
yguenduez <yasinguenduez1@gmail.com>
2024-01-15 21:54:53 +0100

    add a timer to display how long a game takes place

--- src/ui.rs
+++ src/ui.rs
@@ -24,6 +24,69 @@ struct PointDisplayRoot;
 #[derive(Component)]
 struct PointsText;
 
+#[derive(Component)]
+struct TimeText;
+
+#[derive(Component)]
+struct TimeDisplayRoot;
+
+fn setup_time_ui(mut commands: Commands) {
+    let root = commands
+        .spawn((
+            TimeDisplayRoot,
+            NodeBundle {
+                // give it a dark background for readability
+                background_color: BackgroundColor(Color::BLACK.with_a(0.5)),
+                // make it "always on top" by setting the Z index to maximum
+                // we want it to be displayed over all other UI
+                z_index: ZIndex::Global(i32::MAX),
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    // position it at the top-right corner
+                    // 1% away from the top window edge
+                    right: Val::Percent(45.),
+                    top: Val::Percent(1.),
+                    // set bottom/left to Auto, so it can be
+                    // automatically sized depending on the text
+                    bottom: Val::Auto,
+                    left: Val::Auto,
+                    // give it some padding for readability
+                    padding: UiRect::all(Val::Px(4.0)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        ))
+        .id();
+
+    let color = TextStyle {
+        font_size: 16.,
+        color: Color::GREEN,
+        ..Default::default()
+    };
+
+    let time_text = commands
+        .spawn((
+            TimeText,
+            TextBundle {
+                // use two sections, so it is easy to update just the number
+                text: Text::from_sections([
+                    TextSection {
+                        value: "Time Left: ".into(),
+                        style: color.clone(),
+                    },
+                    TextSection {
+                        value: "N/A".into(),
+                        style: color,
+                    },
+                ]),
+                ..Default::default()
+            },
+        ))
+        .id();
+    commands.entity(root).push_children(&[time_text]);
+}
+
 fn setup_points_ui(mut commands: Commands) {
     // create our UI root node
     // this is the wrapper/container for the text
@@ -101,9 +164,11 @@ fn point_text_update_system(
     }
 }
 
-fn display_game_time(q_timer: Query<&mut GameTime>) {
-    if let Ok(time) = q_timer.get_single() {
-        println!("{:?}", time.current_time());
+fn display_game_time(q_timer: Query<&mut GameTime>, mut query: Query<&mut Text, With<TimeText>>) {
+    if let Ok(game_timer) = q_timer.get_single() {
+        for mut text in &mut query {
+            text.sections[1].value = format!("{}", game_timer.current_time().as_secs());
+        }
     }
 }
 
@@ -111,7 +176,7 @@ pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(Startup, setup_points_ui)
+        app.add_systems(Startup, (setup_points_ui, setup_time_ui))
             .add_systems(Update, (point_text_update_system, display_game_time));
     }
 }
